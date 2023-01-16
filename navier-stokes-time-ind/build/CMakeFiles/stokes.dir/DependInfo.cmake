a33e875317b4a17d823403cbf4de8b2467807212
Overlord2410 <mtrigam@gmail.com>
2023-01-16 15:55:53 +0000

    added nonlinear stationary navierstokes

---
+++ navier-stokes-time-ind/build/CMakeFiles/stokes.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jellyfish/shared-folder/PDE/project_with_simo/navier-stokes/navier-stokes-time-ind/src/main-stokes.cpp" "CMakeFiles/stokes.dir/src/main-stokes.cpp.o" "gcc" "CMakeFiles/stokes.dir/src/main-stokes.cpp.o.d"
+  "/home/jellyfish/shared-folder/PDE/project_with_simo/navier-stokes/navier-stokes-time-ind/src/stokes.cpp" "CMakeFiles/stokes.dir/src/stokes.cpp.o" "gcc" "CMakeFiles/stokes.dir/src/stokes.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
